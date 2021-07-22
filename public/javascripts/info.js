435813a6f6b260912a853e2588c316716d1842c0
gcpetri <gcpetri@outlook.com>
2021-07-21 20:02:04 -0500

    info section start

---
+++ public/javascripts/info.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+// eslint-disable-next-line no-unused-vars, func-names
+const copyToClipboard = function (context) {
+  const span = $(context).parent().parent().find('span')[0];
+  const $temp = $('<input>');
+  $('body').append($temp);
+  $temp.val($(span).text()).select();
+  const success = document.execCommand('copy');
+  console.info(success ? 'copied' : 'could not copy');
+  $temp.remove();
+};
